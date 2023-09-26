8c85304fe334daf5759eb5d8c69381ef54cb23d7
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-26 20:25:27 +0200

    commit pre modifice aggiunta menu

--- la-mia-pizzeria-static/Controllers/HomeController.cs
+++ la-mia-pizzeria-static/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace la_mia_pizzeria_static.Controllers
         {
             return View();
         }
+        public IActionResult Menu()
+        {
+            return View();
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
